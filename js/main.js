dedbcb2be1f932d410fa078bd88b42c7f8706612
hatopoppo404 <simamura404@gmail.com>
2026-03-13 20:17:43 +0900

    全体レイアウト

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 'use strict';
 
-import { initModal, showAlert, showConfirm } from './modal-alert.js';
+import { modalMessage } from './modal-alert.js';
 import { initEditor } from './quill.js';
 
-initModal();
 const myEditor = initEditor('#editor');
+
+const testbtn = document.getElementById('openBtn')
+testbtn.addEventListener('click', async () => {
+
+    const result = await modalMessage('title', 'message', false);
+
+    if (result) {
+        alert('done');
+    }
+});
+
--- js/modal-alert.js
+++ js/modal-alert.js
@@ -1,79 +1,109 @@
-export function initModal() {
-    const modal = document.getElementById('modal')
-    const openBtn = document.getElementById('openBtn')
-    const cancelBtn = document.getElementById('cancelBtn')
-
-    openBtn.addEventListener('click', () => {
-        modal.showModal()
-    })
-
-    cancelBtn.addEventListener('click', () => {
-        modal.close()
-    })
-}
-
-
-
-
-
-
-// アラート
-export function showAlert(title, message) {
-    document.getElementById('alertTitle').innerText = title;
-    document.getElementById('alertMessage').innerText = message;
-    document.getElementById('customAlert').classList.add('is-open');
-
-    document.getElementById('alertClose').focus();
-}
-
-// 閉じるボタンのイベント
-document.getElementById('alertClose').addEventListener('click', () => {
-    document.getElementById('customAlert').classList.remove('is-open');
-});
-
-export function showConfirm(title, message) {
+export async function modalMessage(title, message, okOnly) {
     return new Promise((resolve) => {
-        document.getElementById('confirmTitle').innerText = title;
-        document.getElementById('confirmMessage').innerText = message;
-        const modal = document.getElementById('customConfirm');
-        const confirmWindow = document.getElementById('confirmWindow');
 
-        modal.classList.add('is-open');
+        // 変数宣言
+        const modal = document.getElementById('modal');
+        const panel = document.getElementById('modal__panel');
+        document.getElementById('modal__title').innerText = title;
+        document.getElementById('modal__text').innerText = message;
+        const okBtn = document.getElementById('modal__ok');
+        const cancelBtn = document.getElementById('modal__cancel');
 
-        modal.addEventListener('click', (e) => {
-            // クリックされたのが「背景」そのものだった場合（中のウィンドウじゃなくて）
-            if (e.target === modal) {
-                // 揺らすクラスを追加
-                confirmWindow.classList.add('shake-animation');
-
-                // アニメーションが終わったらクラスを外す（次また振れるように）
+        // 関数宣言
+        const onOverlayClick = (e) => {
+            if (!e.target.closest('#modal__panel')) {
+                panel.classList.add('shake-animation');
                 setTimeout(() => {
-                    confirmWindow.classList.remove('shake-animation');
+                    panel.classList.remove('shake-animation');
                 }, 300);
             }
-        });
-
-        const okBtn = document.getElementById('confirmOk');
-        const cancelBtn = document.getElementById('confirmCancel');
-
-        const onOk = () => {
+        };
+        const onOk = (e) => {
+            e.stopPropagation();
             cleanup();
             resolve(true);
         };
-
-        const onCancel = () => {
+        const onCancel = (e) => {
+            e.stopPropagation();
             cleanup();
             resolve(false);
         };
-
-        // 後片付け用関数（イベントを消して閉じる）
         function cleanup() {
             okBtn.removeEventListener('click', onOk);
             cancelBtn.removeEventListener('click', onCancel);
-            modal.classList.remove('is-open');
-        }
+            modal.removeEventListener('click', onOverlayClick);
+            modal.close();
+        };
 
+        // 処理
+        modal.classList.toggle('okOnly', okOnly);
+
+        modal.addEventListener('click', onOverlayClick);
         okBtn.addEventListener('click', onOk);
         cancelBtn.addEventListener('click', onCancel);
+
+        modal.showModal();
     });
 };
+
+
+// // アラート
+// export function showAlert(title, message) {
+//     document.getElementById('alertTitle').innerText = title;
+//     document.getElementById('alertMessage').innerText = message;
+//     document.getElementById('customAlert').classList.add('is-open');
+
+//     document.getElementById('alertClose').focus();
+// }
+
+// // 閉じるボタンのイベント
+// document.getElementById('alertClose').addEventListener('click', () => {
+//     document.getElementById('customAlert').classList.remove('is-open');
+// });
+
+// export function showConfirm(title, message) {
+//     return new Promise((resolve) => {
+//         document.getElementById('confirmTitle').innerText = title;
+//         document.getElementById('confirmMessage').innerText = message;
+//         const modal = document.getElementById('customConfirm');
+//         const confirmWindow = document.getElementById('confirmWindow');
+
+//         modal.classList.add('is-open');
+
+//         modal.addEventListener('click', (e) => {
+//             // クリックされたのが「背景」そのものだった場合（中のウィンドウじゃなくて）
+//             if (e.target === modal) {
+//                 // 揺らすクラスを追加
+//                 confirmWindow.classList.add('shake-animation');
+
+//                 // アニメーションが終わったらクラスを外す（次また振れるように）
+//                 setTimeout(() => {
+//                     confirmWindow.classList.remove('shake-animation');
+//                 }, 300);
+//             }
+//         });
+
+//         const okBtn = document.getElementById('confirmOk');
+//         const cancelBtn = document.getElementById('confirmCancel');
+
+//         const onOk = () => {
+//             cleanup();
+//             resolve(true);
+//         };
+
+//         const onCancel = () => {
+//             cleanup();
+//             resolve(false);
+//         };
+
+//         // 後片付け用関数（イベントを消して閉じる）
+//         function cleanup() {
+//             okBtn.removeEventListener('click', onOk);
+//             cancelBtn.removeEventListener('click', onCancel);
+//             modal.classList.remove('is-open');
+//         }
+
+//         okBtn.addEventListener('click', onOk);
+//         cancelBtn.addEventListener('click', onCancel);
+//     });
+// };
