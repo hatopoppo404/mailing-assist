2aa4ea98728057a39d1c8be6ad1098c4c2887067
hatopoppo404 <simamura404@gmail.com>
2026-03-24 14:25:41 +0900

    quill色追加

--- js/quill.js
+++ js/quill.js
@@ -1,37 +1,38 @@
 // js/editor-setup.js
-export function initEditor(containerId) {
-    const quill = new Quill(containerId, {
-        theme: 'snow',
-        modules: {
-            toolbar: [
-                ['bold', 'italic', 'underline'],
-                [{ 'color': [] }, { 'background': [] }],
-                [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                [{ 'size': ['small', false, 'large', 'huge'] }],
-                ['table'],
-                ['link', 'clean']
-            ],
-            keyboard: {
-                bindings: {
-                    tab: {
-                        key: 9,
-                        handler: function (range) {
-                            this.quill.insertText(range.index, '\t');
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-    });
-    return quill;
-}
+// export function initEditor(containerId) {
+//     const quill = new Quill(containerId, {
+//         theme: 'snow',
+//         modules: {
+//             toolbar: [
+//                 ['bold', 'italic', 'underline'],
+//                 [{ 'color': [] }, { 'background': [] }],
+//                 [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+//                 [{ 'size': ['small', false, 'large', 'huge'] }],
+//                 ['table'],
+//                 ['link', 'clean']
+//             ],
+//             keyboard: {
+//                 bindings: {
+//                     tab: {
+//                         key: 9,
+//                         handler: function (range) {
+//                             this.quill.insertText(range.index, '\t');
+//                             return false;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     });
+//     return quill;
+// }
 
 export const quill = new Quill('#editor', {
     theme: 'snow',
     modules: {
         toolbar: [
             ['bold', 'italic', 'underline'],
+            [{ 'color': [] }, { 'background': [] }],
             [{ 'list': 'ordered' }, { 'list': 'bullet' }],
             [{ 'size': ['small', false, 'large', 'huge'] }],
             ['table'],
